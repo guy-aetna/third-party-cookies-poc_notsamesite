a90e6623e4c19ea51844cbbcd9372dcb2592635e
A436530 <A436530@A436530Z-DEN-OEM.local>
2020-07-14 08:38:32 -0600

    set cookies as iframe or redirect + prettify

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const fs = require('fs');
-const https = require('https');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const https = require("https");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 const port = 3002;
@@ -12,42 +12,71 @@ app.use(cookieParser());
 
 app.use(cors());
 
-app.get('/iframe', (req, res) => {
-    res.sendFile(path.join(__dirname + "/iframe.html"));
-})
+app.get("/iframe", (req, res) => {
+  res.sendFile(path.join(__dirname + "/iframe.html"));
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + "/home.html"));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/home.html"));
+});
 
-app.get('/firstpartycookie', (req, res) => {
-    console.log('req cookies', req.cookies);
+app.get("/firstpartycookie", (req, res) => {
+  console.log("req cookies", req.cookies);
 
-    res.cookie('notsamesite_firstparty', 'testing', {
-            expires: new Date(Date.now() + 900000),
-            sameSite: "none",
-            secure: true,
-            httpOnly: true
-        })
-        .send('test');
-})
+  res
+    .cookie("notsamesite_firstparty", "testing first party", {
+      expires: new Date(Date.now() + 900000),
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    })
+    .send("test");
+});
 
-app.get('/thirdpartycookie', (req, res) => {
-    console.log('req cookies', req.cookies);
+app.get("/redirect", (req, res) => {
+  console.log("req cookies", req.cookies);
 
-    res.cookie('notsamesite_thirdparty', 'testing', {
-            expires: new Date(Date.now() + 900000),
-            sameSite: "none",
-            secure: true,
-            httpOnly: true
-        })
-        .send('test');
-})
+  res.sendFile(path.join(__dirname + "/redirect.html"));
+});
 
-https.createServer({
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-  }, app)
+app.get("/redirect_thirdpartycookie", (req, res) => {
+  console.log("req cookies", req.cookies);
+
+  res
+    .cookie("notsamesite_redirect_thirdparty", "testing redirect", {
+      domain: ".notsamesite.com",
+      expires: new Date(Date.now() + 900000),
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    })
+    .send("redirect");
+});
+
+app.get("/iframe_thirdpartycookie", (req, res) => {
+  console.log("req cookies", req.cookies);
+
+  res
+    .cookie("notsamesite_iframe_thirdparty", "testing iframe", {
+      domain: ".notsamesite.com",
+      expires: new Date(Date.now() + 900000),
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    })
+    .send("test");
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
   .listen(port, function () {
-    console.log(`Example app listening on ${port} Go to https://localhost:${port}/`);
-  })
+    console.log(
+      `Example app listening on ${port} Go to https://localhost:${port}/`
+    );
+  });
